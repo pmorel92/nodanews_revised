511e8fa35eff5d0f7f8a35237340bc834acddd69
pmorel92 <philippe.a.b.morel@gmail.com>
2017-01-17 11:22:34 -0800

    heroku a third time

--- nodanews_project/wsgi.py
+++ nodanews_project/wsgi.py
@@ -1,23 +1,8 @@
-"""
-WSGI config for nodanews_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nodanews.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nodanews_project.settings")
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
-
 application = DjangoWhiteNoise(application)

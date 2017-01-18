30e079ab7ed1725bc9f97e393e2e8ca93a168b99
pmorel92 <philippe.a.b.morel@gmail.com>
2017-01-18 14:06:53 -0800

    static filesh a

--- nodanews_project/wsgi.py
+++ nodanews_project/wsgi.py
@@ -2,7 +2,7 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nodanews_project.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise import WhiteNoise
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application, root='/nodanews_project/staticfiles')
+application = WhiteNoise(application, root='/nodanews_project/staticfiles')

35b132788ac18f1f2df639105393e8f843419978
pmorel92 <philippe.a.b.morel@gmail.com>
2017-01-18 14:02:56 -0800

    static filesh a

--- nodanews_project/wsgi.py
+++ nodanews_project/wsgi.py
@@ -5,4 +5,4 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='/nodanews_project/staticfiles')
+application = DjangoWhiteNoise(application, root='/nodanews_project/staticfiles')

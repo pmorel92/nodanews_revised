786d856fbe777911a207f60cde6fbe6b5e62f320
Philippe Morel <philippe.a.b.morel@gmail.com>
2017-01-18 15:39:50 -0800

    Update settings.py

--- nodanews_project/settings.py
+++ nodanews_project/settings.py
@@ -124,11 +124,11 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'staticfiles'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 # Simplified static file serving.

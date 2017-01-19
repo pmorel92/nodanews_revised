2da4885674423bc908b83dd5130eb983b9ffd5a6
Philippe <philippe.a.b.morel@gmail.com>
2017-01-19 23:48:16 +0000

    first cloud9 commit

--- nodanews_project/settings.py
+++ nodanews_project/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'nodanews'
+    'nodanews',
+    'storages',
 ]
 
 MIDDLEWARE_CLASSES = [

94d84a974bee8a2dd52d65b0c0234bd52f386ab2
pmorel92 <philippe.a.b.morel@gmail.com>
2017-01-26 05:17:03 +0000

    another test

--- nodanews_project/settings.py
+++ nodanews_project/settings.py
@@ -19,7 +19,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, 'nodanews/templates').replace('\\','/'),
+    os.path.join(PROJECT_ROOT, 'templates/nodanews').replace('\\','/'),
 )
 
 

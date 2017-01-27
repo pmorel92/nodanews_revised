fdee3fa545f29bf263380cb2332c85a26ba13c4f
pmorel92 <philippe.a.b.morel@gmail.com>
2017-01-27 23:06:20 +0000

    update views

--- nodanews/views.py
+++ nodanews/views.py
@@ -28,12 +28,11 @@ def media_dir(request):
 
 def node(request, node_id):
 	node = get_object_or_404(Node, pk=node_id)
-	ref = Node.id
-	link_source = Link_Sources.objects.get(Link_Sources.node_ref_id=ref)
-	link_wikipedia = Link_Wikipedia.objects.filter()
-	link_academic = Link_Academic.objects.filter(node_id=Link_Academic.node_ref.id)
-	link_node = Link_Nodes.objects.filter(node_id=Link_Nodes.node_ref.id)
-	link_video = Link_Video.objects.filter(node_id=Link_Video.node_ref.id)
+	link_source = Link_Sources.objects.filter(node_ref=node)
+	link_wikipedia = Link_Wikipedia.objects.filter(node_ref=node)
+	link_academic = Link_Academic.objects.filter(node_ref=node)
+	link_node = Link_Nodes.objects.filter(node_ref=node)
+	link_video = Link_Video.objects.filter(node_ref=node)
 	return render(request, 'nodanews/node.html', {'node': node, 'link_source': link_source, 'link_wikipedia': link_wikipedia, 'link_academic': link_academic ,'link_node': link_node, 'link_video': link_node})
 
 

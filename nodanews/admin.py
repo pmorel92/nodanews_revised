3337eef3557e7d8c3216ab9e7cc3573d1f78c2b3
Philippe Morel <philippe.a.b.morel@gmail.com>
2017-02-06 11:37:01 -0800

    Update admin.py

--- nodanews/admin.py
+++ nodanews/admin.py
@@ -42,19 +42,19 @@ class Index_Admin(admin.ModelAdmin):
 	        'fields': ('index_image', 'headline', 'headline_2', 'headline_3', 'headline_4')       
 	    }),
         ('Links', {
-            'classes': ('collapse'),
+            'classes': ('collapse',),
             'fields': ('Link_1', 'Link_1_title', 'Link_2', 'Link_2_title', 'Link_3', 'Link_3_title')
         }),
         ('Links', {
-            'classes': ('collapse'),
+            'classes': ('collapse',),
             'fields': ('Link_21', 'Link_21_title', 'Link_22', 'Link_22_title', 'Link_23', 'Link_23_title')
         }),
         ('Links', {
-            'classes': ('collapse'),
+            'classes': ('collapse',),
             'fields': ('Link_31', 'Link_31_title', 'Link_32', 'Link_32_title', 'Link_33', 'Link_33_title')
         }),
         ('Links', {
-            'classes': ('collapse'),
+            'classes': ('collapse',),
             'fields': ('Link_41', 'Link_41_title', 'Link_42', 'Link_42_title', 'Link_43', 'Link_43_title')
         }),
 	
